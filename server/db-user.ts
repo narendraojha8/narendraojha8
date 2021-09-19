c0427102b8de1b477b89fc76b35d3c33f6515fd6
unknown <narendraojha8@gmail.com>
2021-09-19 11:48:59 +0530

    1-signup

---
+++ server/db-user.ts
@@ -0,0 +1,5 @@
+export interface dbUser{
+    id:number;
+    email:string;
+    password:string;
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,25 @@
+import {  HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+// import {shareReplay } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../model/users';
+
+export const ANONYMOUS_USER:User={
+  id:undefined,
+  email:''
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService  {
+   private subject = new BehaviorSubject<User>(ANONYMOUS_USER);
+   user$:Observable<User> = this.subject.asObservable();
+   isLoggedIn$:Observable<boolean> = this.user$.map(user=>!!user.id);
+   isLoggedOut$:Observable<boolean> = this.isLoggedIn$.map(user=>!user);
+
+  constructor(private http:HttpClient) { }
+  signUp(email:string,password:string){
+   return this.http.post<User>('/api/signup',{email,password}).shareReplay();
+   
+  }
+}
